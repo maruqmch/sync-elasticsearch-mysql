c5437d61ab15f28a50cd0b3e70e9924e3131ef57
maruqmch <marqumch@gmail.com>
2023-03-01 19:13:16 +0500

    Update from-scratch.sql

--- volumes/logstash/config/queries/from-scratch.sql
+++ volumes/logstash/config/queries/from-scratch.sql
@@ -1,1 +1,11 @@
-SELECT * FROM books.books
+SELECT
+	yovo_tbl_leads.*, 
+	SUBSTRING_INDEX(yovo_tbl_leads.fbcampaignname, '_',1) AS campaignName, 
+	SUBSTRING_INDEX(yovo_tbl_leads.fbcampaignname, '_',-2) AS campaignRegion, 
+	yovo_tbl_products_services.`name`
+FROM
+	yovo_tbl_leads
+	INNER JOIN
+	yovo_tbl_products_services
+	ON 
+		yovo_tbl_leads.product = yovo_tbl_products_services.id
